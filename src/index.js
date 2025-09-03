cf6b6cddb9241c90cdbd25c60bdbabd73a218f93
Hyan figueiredo <109700731+HSfigueiredo@users.noreply.github.com>
2025-09-03 13:21:09 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,19 +3,11 @@ const axios = require('axios');
 
 
 const client = new Client({
-    authStrategy: new LocalAuth(),
+    authStrategy: new LocalAuth({ clientId: "bot1" }),
     puppeteer: {
         headless: true,
-        args: [
-            "--no-sandbox",
-            "--disable-setuid-sandbox",
-            "--disable-dev-shm-usage",
-            "--disable-accelerated-2d-canvas",
-            "--no-first-run",
-            "--no-zygote",
-            "--single-process",
-            "--disable-gpu"
-        ],
+        executablePath: '/usr/bin/google-chrome',
+        args: ['--no-sandbox', '--disable-setuid-sandbox', '--disable-dev-shm-usage']
     }
 });
 
