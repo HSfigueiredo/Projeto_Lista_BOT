1aedb39433c6de97925d78ca339ca4131f9f460f
Hyan figueiredo <109700731+HSfigueiredo@users.noreply.github.com>
2025-09-03 13:47:26 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,7 +6,6 @@ const client = new Client({
     authStrategy: new LocalAuth({ clientId: "bot1" }),
     puppeteer: {
         headless: true,
-        executablePath: process.env.PUPPETEER_EXECUTABLE_PATH,
         args: ['--no-sandbox', '--disable-setuid-sandbox', '--disable-dev-shm-usage']
     }
 });
