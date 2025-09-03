0c87205366034d2a185feae8d268394c3c07a6fa
Hyan figueiredo <109700731+HSfigueiredo@users.noreply.github.com>
2025-09-03 12:10:31 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,8 +3,22 @@ const axios = require('axios');
 
 
 const client = new Client({
-    authStrategy: new LocalAuth()
+    authStrategy: new LocalAuth(),
+    puppeteer: {
+        headless: true,
+        args: [
+            "--no-sandbox",
+            "--disable-setuid-sandbox",
+            "--disable-dev-shm-usage",
+            "--disable-accelerated-2d-canvas",
+            "--no-first-run",
+            "--no-zygote",
+            "--single-process",
+            "--disable-gpu"
+        ],
+    }
 });
+
 
 client.on('qr', async (qr) => {
 
